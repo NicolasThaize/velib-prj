b59cab8095341113c35e3325e0b1f285f7d6a4a7
Nicolas git kraken <nicolas.thaize@ynov.com>
2023-02-18 12:17:49 +0100

    cleaning code

--- kafka/producer.py
+++ kafka/producer.py
@@ -32,17 +32,15 @@ def store_to_kafka(offline=False): # main
         for station_data in velibs_data_filtered: # For each velib station
             station_data['fields']['duedate'] = convert_due_date_to_timestamp(station_data['fields']['duedate']) # Formatting date format to allow spark timestamp usage
             station_data['fields']['timestamp'] = station_data['fields'].pop('duedate') # Renaming 'duedate' column to 'timestamp'
-            print(station_data)
             station_code = station_data['fields']['stationcode']
             station_last_refresh = station_data['fields']['timestamp']
-        
             if station_code in LAST_TIMESTAMP: # If key already exist
                 if date1_greater_date2(parse_string_date(station_last_refresh, KAFKA_TIMESTAMP_FORMAT), parse_string_date(LAST_TIMESTAMP[station_code], KAFKA_TIMESTAMP_FORMAT)): # If date from API is greater than kafka's last stored one
-                    print(station_data['fields'], " UPDATED")
+                    print(station_data['fields']['stationcode'], " UPDATED")
                     producer.send('test1', station_data['fields'])
                     LAST_TIMESTAMP[station_code] = station_last_refresh # Update last station timestamp field
             else: # If key does not exist
-                print(station_data['fields'], " CREATED")
+                print(station_data['fields']['stationcode'], " CREATED")
                 producer.send('test1', station_data['fields'])
                 LAST_TIMESTAMP[station_code] = station_last_refresh # Create last station timestamp field
         sleep(60) # Refresh data every minutes
